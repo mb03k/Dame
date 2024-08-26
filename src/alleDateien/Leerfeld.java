b65ca4f4eb20eecb3f014f6b43fc6eab5e2d0145
matthes <matthesboettcher03@gmail.com>
2024-08-26 15:41:45 +0200

    Warnungen verringert

--- src/alleDateien/Leerfeld.java
+++ src/alleDateien/Leerfeld.java
@@ -1,18 +1,9 @@
 package alleDateien;
 
 public class Leerfeld extends Spielstein {
-    private int pos_x;
-    private int pos_y;
 
-    private int farbe = 0;
+    // Klasse existiert nur um Typsicherheit zu gewährleisten?
 
     public Leerfeld(int pos_x, int pos_y) {
-        this.pos_x = pos_x;
-        this.pos_y = pos_y;
-    }
-
-    @Override
-    public int getFarbe() {
-        return farbe;
     }
 }
