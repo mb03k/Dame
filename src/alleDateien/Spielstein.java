baead0463395c490166221198f774d4a75ea525d
Hannes2806 <hannes.martens1999@gmail.com>
2024-08-27 16:31:25 +0200

    Update Spielstein.java

--- src/alleDateien/Spielstein.java
+++ src/alleDateien/Spielstein.java
@@ -5,22 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 
 public abstract class Spielstein {
-    public void setzeFigur() {}
-    public void schlageFigur() {}
     public boolean istBewegungsfaehig() {
         return true;
     }
 
     public List<int[]> getBewegungsziele() {
         return new ArrayList<>();
-    }
-
-    public List<List<Integer>> getBewegungspfade() {
-        return new ArrayList<>();
-    }
-
-    public int getLaufrichtung() {
-        return 0;
     }
 
     public int getFarbe() {
@@ -37,5 +27,9 @@ public abstract class Spielstein {
 
     public List<List<int[]>> getBewegungspfadeSchlagen() {
         return null;
+    }
+
+    public Boolean getBewegungsfaehig() {
+        return true;
     }
 }
