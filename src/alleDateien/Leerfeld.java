f3e07fd6bb3dd0953cb7c341d40d49f3e224c7c4
Hannes2806 <hannes.martens1999@gmail.com>
2024-08-28 12:34:32 +0200

    Update Leerfeld.java

--- src/alleDateien/Leerfeld.java
+++ src/alleDateien/Leerfeld.java
@@ -2,8 +2,7 @@ package alleDateien;
 
 public class Leerfeld extends Spielstein {
 
-    // Klasse existiert nur um Typsicherheit zu gewährleisten?
-
+    // Klasse nur für Typsicherheit
     public Leerfeld(int pos_x, int pos_y) {
     }
 }
